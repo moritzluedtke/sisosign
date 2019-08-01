e81355e46b33bf1e548045ae95595f39cfd0aa75
ML (Desktop PC) <moritz.luedtke@gmx.de>
2019-08-01 21:50:38 +0200

    Added redirect to index.html in server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // server.js
 const express = require('express');
 const app = express();
+const path = require('path');
+
 
 // Run the app by serving the static files
 // in the dist directory
@@ -9,3 +11,9 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
