0c95eba72f6ce0e24a00a868ccc1fdbb23bb1c41
ML (Desktop PC) <moritz.luedtke@gmx.de>
2019-08-02 06:33:09 +0200

    Corrected path to built app

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/SISOSIG'));
 
 // Start the app by listening on the default
 // Heroku port
@@ -15,5 +15,5 @@ app.listen(process.env.PORT || 8080);
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/SISOSIG/index.html'));
 });
